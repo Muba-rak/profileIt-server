0ba00c36c3a8f7dad04f776c3dd86265e42b70ea
Mubarak Oladapo <oladapomubarak@gmail.com>
2023-05-28 15:58:23 +0100

    added more fields to models

--- models/status.js
+++ models/status.js
@@ -16,6 +16,35 @@ const postSchema = new Schema(
       type: String,
       required: [true, "please enter a password"],
     },
+    fullname: {
+      type: String,
+      required: [true, "Please enter a name"],
+    },
+    mothersmaidenname: {
+      type: String,
+      required: [true, "Please enter your Mother's maiden Name"],
+    },
+    address: {
+      type: String,
+      required: [true, "Please enter your address"],
+    },
+    gender: {
+      type: String,
+      required: true,
+      enum: ["male", "female", "others"],
+    },
+    hobby: {
+      type: String,
+      required: [true, "Please enter your hobbies"],
+    },
+    height: {
+      type: Number,
+      required: [true, "Please enter your height"],
+    },
+    dob: {
+      type: Date,
+      required: [true, "Enter your date of birth"],
+    },
     createdBy: {
       type: mongoose.Types.ObjectId,
       ref: "User",
